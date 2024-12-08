d7b61e0d67750f071484d1c2879daaed826f3c5a
Priyankanegi28 <negipriyanka949@gmail.com>
2024-12-08 23:51:33 +0530

    Added login function

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ function signup(userName) {
 }
 
 module.exports = { signup };
+
+function login(userName, password) {
+    const users = ["Alice", "Bob", "Charlie"]; 
+    const correctPassword = "Emp@123"; 
+  
+    if (!users.includes(userName)) {
+      return "User Not Found, Please Signup";
+    } else if (password !== correctPassword) {
+      return "Wrong Password....";
+    } else {
+      return "Login Successful...";
+    }
+  }
+  
+  module.exports = { signup, login };
